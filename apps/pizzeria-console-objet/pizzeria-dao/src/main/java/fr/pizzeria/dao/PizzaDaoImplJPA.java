b2fc336943ff08e66052a8920c7563e4e9ebd8ce
Paola Lambroni <paolalambroni@gmail.com>
2017-03-10 17:26:21 +0100

    jpa update pizza

--- apps/pizzeria-console-objet/pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoImplJPA.java
+++ apps/pizzeria-console-objet/pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoImplJPA.java
@@ -9,7 +9,10 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.persistence.RollbackException;
 
+import fr.pizzeria.exception.SavePizzaException;
+import fr.pizzeria.exception.UpdatePizzaException;
 import fr.pizzeria.model.Pizza;
 
 public class PizzaDaoImplJPA implements Dao<Pizza, String> {
@@ -43,20 +46,44 @@ public class PizzaDaoImplJPA implements Dao<Pizza, String> {
 			et.begin();
 			em.persist(pizza);
 			et.commit();
-		} catch (Exception up) {
+		} catch (RollbackException e) {
 			et.rollback();
-			throw up;
+			throw new SavePizzaException(e);
 		}
 	}
 
 	@Override
-	public void update(String codePlat, Pizza plat) {
-		// TODO Auto-generated method stub
+	public void update(String codePizza, Pizza newPizza) {
+		List<Pizza> listPizzas = new ArrayList<>();
+		Query query = em.createQuery("SELECT p FROM Pizza p	WHERE p.code=:codePizza").setParameter("codePizza",
+				codePizza);
+		for (Object obj : query.getResultList()) {
+			listPizzas.add((Pizza) obj);
+		}
+		Pizza p = listPizzas.get(0);
 
+		if (p != null) {
+
+			listPizzas.get(0).setCode(newPizza.getCode());
+			listPizzas.get(0).setNom(newPizza.getNom());
+			listPizzas.get(0).setPrix(newPizza.getPrix());
+			listPizzas.get(0).setCategoriePizza(newPizza.getCategoriePizza());
+
+		}
+
+		EntityTransaction et = em.getTransaction();
+		try {
+			et.begin();
+			em.merge(listPizzas.get(0));
+			et.commit();
+		} catch (RollbackException e) {
+			et.rollback();
+			throw new UpdatePizzaException(e);
+		}
 	}
 
 	@Override
-	public void delete(String codePlat) {
+	public void delete(String codePizza) {
 		// TODO Auto-generated method stub
 
 	}
