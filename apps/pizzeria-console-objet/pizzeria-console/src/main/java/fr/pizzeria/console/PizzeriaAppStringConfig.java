a3056373f250c975fbe26ec5ad3dc71d32086f95
Paola Lambroni <paolalambroni@gmail.com>
2017-03-29 15:06:03 +0200

    Spring Jbdc h2 + tests associés

--- apps/pizzeria-console-objet/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAppStringConfig.java
+++ apps/pizzeria-console-objet/pizzeria-console/src/main/java/fr/pizzeria/console/PizzeriaAppStringConfig.java
@@ -3,11 +3,14 @@ package fr.pizzeria.console;
 import java.util.Scanner;
 import java.util.logging.Level;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import fr.pizzeria.dao.DaoClient;
 import fr.pizzeria.dao.DaoPizza;
@@ -27,11 +30,9 @@ public class PizzeriaAppStringConfig {
 
 	@Bean
 	public DaoPizza<Pizza, String> implementationPizzaDao() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		dataSource.setUrl("jdbc:mariadb://localhost:3306/pizzeria?useSSL=false");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		DataSource dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript("init-schema.sql").build();
+
 		return new PizzaDaoImplJdbcTemplate(dataSource);
 	}
 
